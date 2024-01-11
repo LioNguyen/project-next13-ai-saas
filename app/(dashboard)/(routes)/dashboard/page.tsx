dd957273201c8cc29a4017f0cf68ea7f422e537d
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-01-11 10:44:03 +0700

    feat: create clerk authentication

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <p>Dashboard Page</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
